6266618a6799279e0da3e3b685ddab7522c64e8a
Stefan Tomic <stefantomic@Stefans-MacBook-Pro.local>
2024-03-09 22:29:15 +0100

    Cocoapods added

--- GitHubUserTest/GitHubUserTest/HomeViewController.swift
+++ GitHubUserTest/GitHubUserTest/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  GitHubUserTest
 //
 //  Created by Stefan Tomic on 9.3.24..
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
     }
-
-
+    
 }
-
