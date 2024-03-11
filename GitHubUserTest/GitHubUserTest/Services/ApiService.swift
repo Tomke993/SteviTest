02aca3cd7aa7f92c2b1b26881f3b0daf193cb462
Stefan Tomic <stefantomic@Stefans-MacBook-Pro.local>
2024-03-11 20:57:46 +0100

    ApiService refactored

--- GitHubUserTest/GitHubUserTest/Services/ApiService.swift
+++ GitHubUserTest/GitHubUserTest/Services/ApiService.swift
@@ -9,8 +9,8 @@ import Alamofire
 
 class ApiService {
     
-    private let token = ""
-    let username = ""
+    var token = ""
+    var username = ""
     
     static let shared = ApiService()
     
@@ -57,5 +57,50 @@ class ApiService {
                 print("ERROR \n\(error)")
             }
         })
+    }
+    
+    func getAuthCode(complition: @escaping (AuthBean) -> ()) {
+        let clientId = "4993dd894c1b78521445"
+        let request = AF.request("https://github.com/login/device/code", method: .post, parameters: ["client_id" : clientId], encoder: JSONParameterEncoder.default)
+        request.validate()
+        request.response { response in
+            switch response.result {
+            case let .success(data):
+                let dataString = String(data: data!, encoding: .utf8)
+                let params = dataString?.split(separator: "&")
+                let deviceCode = String(params?[0].replacing("device_code=", with: "") ?? "")
+                let expires = Int(params?[1].replacing("expires_in=", with: "") ?? "0") ?? 0
+                let interval = Int(params?[2].replacing("interval=", with: "") ?? "0") ?? 0
+                let userCode = String(params?[3].replacing("user_code=", with: "") ?? "")
+                let authBean = AuthBean(device_code: deviceCode, user_code: userCode, expires_in: expires, interval: interval)
+                print("authBean: \n\(authBean)")
+                complition(authBean)
+            case let .failure(error):
+                print("ERROR \n\(error)")
+            }
+        }
+    }
+    
+    func getAccessToken(deviceCode: String, complition: @escaping () -> ()) {
+        let clientId = "4993dd894c1b78521445"
+        let grantType = "urn:ietf:params:oauth:grant-type:device_code"
+        let parameters = ["device_code" : deviceCode, "client_id" : clientId, "grant_type" : grantType]
+        let request = AF.request("https://github.com/login/oauth/access_token", method: .post, parameters: parameters, encoder: JSONParameterEncoder.default)
+        request.validate()
+        request.response { response in
+            switch response.result {
+            case let .success(data):
+                let dataString = String(data: data!, encoding: .utf8)
+                if let resultString = dataString, !resultString.contains("error=") {
+                    let params = dataString?.split(separator: "&")
+                    let accessToken = String(params?[0].replacing("access_token=", with: "") ?? "")
+                    self.token = accessToken
+                    print("Access Token: \(accessToken)")
+                    complition()
+                }
+            case let .failure(error):
+                print("ERROR \n\(error)")
+            }
+        }
     }
 }
